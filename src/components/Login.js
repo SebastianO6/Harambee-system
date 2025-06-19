6688d0ebd6ab039cc5f416032f96b324fe1208d0
Bill <bill.omondi@student.moringaschool.com>
2025-06-19 10:57:47 +0300

    Form styling added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService } from '../services/auth';
+import './Login.css';
 
 
 export default function Login() {
